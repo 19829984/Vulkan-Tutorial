dafa8dfd3b9a61c0ce380d68f440ea647358b519
Bowen Wu <bowenwu741@gmail.com>
2025-12-12 01:27:26 +0900

    Shader modules chapter

---
+++ code/shaders/compile.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+set -e
+compileOutput=$(slangc triangle.slang -target spirv -profile spirv_1_4 -emit-spirv-directly -fvk-use-entrypoint-name -entry vertMain -entry fragMain -o slang.spv)
+echo "$compileOutput"
